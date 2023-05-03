390db38e7b01087a8d268fc022eb4d504af334e4
Steven McCombe <stevenmccombe93@gmail.com>
2023-05-03 19:34:17 -0400

    algo problem solving

---
+++ Section 4 Prob Solving Approach/file.js
@@ -0,0 +1,8 @@
+//What is an algorithm?
+//A process or set of steps to accompolish a certain task.
+
+//Almost everything you do in programing involves some kind of algorithm.
+
+//How do you improve?
+//1. Devise a plan for solving problems.
+//2
