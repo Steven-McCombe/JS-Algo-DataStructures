d9ab6bdb0b713cf605af3e4da121e2d895f3751d
Steven McCombe <stevenmccombe93@gmail.com>
2023-05-03 20:06:27 -0400

    Understanding the problem

--- Section 4 Prob Solving Approach/file.js
+++ Section 4 Prob Solving Approach/file.js
@@ -5,4 +5,51 @@
 
 //How do you improve?
 //1. Devise a plan for solving problems.
-//2
+//2. Master common problem solving problems. 
+
+//Problem solving strategies
+//1. Understand the Problem
+//2. Explore concrete examples
+//3. Break it down
+//4. Solve/Simplify the problem
+//5. Look back and refactor
+
+//--------------UNDERSTAND THE PROBLEM -------------
+//Reference Book - George Polya - How to solve it
+//1. Can I restate the problem in my own words?
+//2. What are the inputs that go into the problem
+//3. WHat are the outputs that should come from the solution.
+//4. Can the outputs be determined from the inputs? In other words, Do I have enough information to solve.
+//5. How should i label the important pieces.
+
+// Write a function which takes two numbers and returns their sum.
+//1. Implement Addition
+//2. ints, floats, string?
+//3. ints float string?
+//4. What happens if they only give us one number, or more than two
+//5. Input, addition, total
+
+
+//-------------- EXPLORE CONCRETE EXAMPLES -------------
+//Coming up with examples can help you understand the problem better.
+// Examples also provide sanity checks that your eventual solution works how it should. 
+// User Stories, Unit Tests.
+
+// Start with simple examples.
+// Progress to more complex examples
+// Explore ecamples with empty inputs
+// Explore ecamples with invalid inputs
+
+//Write a function which takes in a string and returns counts of each character in the string.
+
+charCount("aaaa"); //{a:4}
+charCount("hello"); //{h:1, e:1, l:2, o:1}
+
+
+//-------------- BREAK IT DOWN -------------
+//Take the steps of the problem and write them down. Pseudocode. 
+// Explicitly write out the steps you need to take. - This forces you to think about the code before you write it. 
+// It also helps you catch any lingering conceptual issues or misunderstandings before you dive in. 
+
+//-------------- SOLVE/SIMPLIFY THE PROBLEM -------------
+//-------------- LOOK BACK AND REFACTOR -------------
