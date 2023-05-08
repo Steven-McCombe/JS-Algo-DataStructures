1080cdf9d5630d1f9f651878ff732e8feeac8e21
Steven McCombe <stevenmccombe93@gmail.com>
2023-05-08 10:58:00 -0400

    refactored frequency counter algo to O(n)

--- Section 5/frequencyCounter.js
+++ Section 5/frequencyCounter.js
@@ -27,3 +27,46 @@ function badSame(arr1, arr2) {
 
 console.log(badSame([2,4,6,8],[4,16,36,64])) //true
 console.log(badSame([2,4,6,8],[4,16,36,63])) //false
+
+
+//Lets refactor and make this O(n)
+
+function same(arr1, arr2) {
+      //initial check to see if the arrays are the same length. if not return false.
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    //Declare two variables as empty objects.
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    // Iterate through arr1 and count the frequency of each value.
+    // Store the value as a key in frequencyCounter1 with its frequency as the value.
+    for (let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) +1
+    }
+
+    // Iterate through arr2 and count the frequency of each value.
+    // Store the value as a key in frequencyCounter2 with its frequency as the value.
+    for (let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) +1
+    }
+
+    // Iterate through the keys in frequencyCounter1.
+    for (let key in frequencyCounter1){
+        // Check if the square of the key is present in frequencyCounter2.
+        // If not, return false.
+        if (!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        // Check if the frequency of the squared key in frequencyCounter2 is equal to the frequency of the key in frequencyCounter1.
+        // If not, return false.
+        if (frequencyCounter2[key **2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    // If all checks pass, return true.
+    return true 
+}
+
+console.log(same([2,4,6,8],[4,16,36,64])) //true
+console.log(same([2,4,6,8],[4,16,36,63])) //false
