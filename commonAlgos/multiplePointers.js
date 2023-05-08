73434f22dc17666b341b9b7be2a9679251252f0c
Steven McCombe <stevenmccombe93@gmail.com>
2023-05-08 12:18:33 -0400

    refactored version of the multiple pointer algo o(n)

--- commonAlgos/multiplePointers.js
+++ commonAlgos/multiplePointers.js
@@ -32,3 +32,29 @@ console.log(sumZeroBad([-2,0,1,3])) // undefined
 console.log(sumZeroBad([1,2,3])) // undefined
 
 //------------------- Refactored ---------------------
+function sumZero(arr){
+    // Initialize two pointers, left and right, to the first and last elements of the input array, respectively.
+    let left = 0
+    let right = arr.length -1
+    // Continue the loop until the left pointer is less than the right pointer.
+    while (left < right) {
+        // Calculate the sum of the values at the left and right pointers.
+        let sum = arr[left] + arr[right];
+    
+        // Check if the calculated sum is equal to zero.
+        if (sum === 0) {
+            // If the sum is zero, return an array containing the values at the left and right pointers.
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            // If the sum is greater than zero, decrement the right pointer to try a smaller value.
+            right--;
+        } else{
+            // If the sum is less than zero, increment the left pointer to try a larger value.
+            left++;
+        }
+    }
+    // If the function reaches this point, no pair of elements with a sum of zero has been found. The function will implicitly return undefined.
+
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10]))
