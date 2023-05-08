7b340a4b60accf033ac22c8898278df231cde211
Steven McCombe <stevenmccombe93@gmail.com>
2023-05-08 18:43:52 -0400

    divide and conquer algos - Binary Search.

--- commonAlgos/divideAndConquer.js
+++ commonAlgos/divideAndConquer.js
@@ -13,7 +13,7 @@
 //If the value is not found, return -1
 
 //Naive Approach
-
+//Time complexity O(n)
 function linearSearch (arr, val){
     for (let i = 0; i < arr.length; i++){
         if(arr[i] === val){
@@ -26,3 +26,28 @@ function linearSearch (arr, val){
 console.log(linearSearch([1,2,3,4,5,6],4)) // 3
 console.log(linearSearch([1,2,3,4,5,6],6)) // 5 
 console.log(linearSearch([1,2,3,4,5,6],11)) //-1
+
+//Refactored version
+//Time Complexity Log(N);
+
+function binarySearch(array, val){
+
+    let min = 0
+    let max = array.length -1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) /2);
+        let currentElement = array[middle];
+
+        if (array[middle] < val) {
+            max = middle + 1
+        }
+        else if (array[middle] > val) {
+            max = middle -1
+        } 
+        else {
+            return middle;
+        }
+    }
+    return -1; 
+}
